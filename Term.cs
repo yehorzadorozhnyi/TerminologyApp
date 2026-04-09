1802236d3e1887d919ad7c06e8a6273ba353dbde
Yehor-Zadorozhnyi <yehor.zadorozhnyi@nure.ua>
2026-04-09 15:49:39 +0300

    Додано модель даних для опису терміна.

---
+++ Term.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TerminologyApp
+{
+    internal class Term
+    {
+        public string Name { get; set; }
+        public string Definition { get; set; }
+        public List<string> References { get; set; }
+
+        public Term(string name, string definition, List<string> references)
+        {
+            Name = name;
+            Definition = definition;
+            References = references;
+        }
+    }
+}
